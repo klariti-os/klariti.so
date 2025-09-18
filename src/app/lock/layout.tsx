d7bba0722ae4cd5f61e6f0e58e5613829cf9d513
ignasxv <ignasxv@gmail.com>
2025-09-18 10:55:19 -0400

    chore: preserve hurdation errors

--- src/app/lock/layout.tsx
+++ src/app/lock/layout.tsx
@@ -4,10 +4,8 @@ export default function LockLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
       <body className="font-mono antialiased bg-[#d9d9d9] selection:bg-white/10">
         {children}
       </body>
-    </html>
   )
 }
