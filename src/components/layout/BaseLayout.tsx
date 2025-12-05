e1dbbc9d99b481c8be9f9cdd25ac861ac0f9cddc
ignasxv <62844231+ignasxv@users.noreply.github.com>
2025-12-04 23:20:21 -0500

    ui: removed the social icons

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -5,7 +5,7 @@ const BaseLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="relative flex flex-col justify-center min-h-screen pt-20">
       <div
-        className="absolute inset-0 bg-no-repeat bg-center opacity-60 bg-fixed"
+        className="absolute inset-0 bg-no-repeat bg-center bg-fixed"
         style={{
           backgroundImage: `url(/images/pc-land2.png)`,
           backgroundSize: "2000px 2000px",
