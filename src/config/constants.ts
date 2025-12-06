3988be3669b0585197e9151aa461c1907ece6c9a
ignasxv <62844231+ignasxv@users.noreply.github.com>
2025-12-05 23:18:05 -0500

    ui: prepping for staging environment

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
-export const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV || process.env.NODE_EN || 'production';
-export const IS_PRODUCTION = APP_ENV === 'production';
+export const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV || process.env.NODE_EN || 'development';
+export const IS_PRODUCTION = APP_ENV === 'development';
 
 export const ROUTES = {
   AUTH_REDIRECT: IS_PRODUCTION ? '/join' : '/auth',
